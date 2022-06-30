1672e8761b0a9ba14e86f97b9818d4c1fd8abb44
sabrinaZait <zsabrina941@outlook.fr>
2022-06-30 16:37:34 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from './components/Main';
+import './components/style.css';
+
+function App() {
+  return (
+    <>
+      <Main/>
+    </>
+  );
+}
+
+export default App;
